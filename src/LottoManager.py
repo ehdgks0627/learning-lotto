725875349e3248d71f356c22df66aa3c33012258
sprout <ehdgks0627>
2017-12-27 14:04:04 +0900

    Added Sample Code in main

--- src/LottoManager.py
+++ src/LottoManager.py
@@ -6,29 +6,49 @@ class LottoManager:
     log_level_list = ("DISABLE", "DEBUG")
     __version__ = 0.1
 
-    def __init__(self):
-        self.set_log_level("DISABLE")
+    def __init__(self, log_level="DISABLE"):
+        self.set_log_level(log_level)
         self.saver = FileSaver(self.log_level)
-        self.lotto_list = self.saver.load_lotto_data()
+        self.lotto_data = self.saver.load_lotto_data()
 
     def get_lotto(self, drwNo):
         url = "http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=%d"%(drwNo)
-        if log_level == "DEBUG":
+        if self.log_level == "DEBUG":
             print("[+] Request %s"%(url))
         response = requests.get(url)
-        if log_level == "DEBUG":
+        if self.log_level == "DEBUG":
             print("[+] Response Status Code is %d"%(response.status_code))
         if response.status_code != self.HTTP_OK:
             return None
         else:
             data = response.json()
+            if data["returnValue"] == "fail":
+                return None
             info = {"totalSell": data["totSellamnt"],
                     "winnerCount": data["firstPrzwnerCo"],
                     "bonusNum": data["bnusNo"],
-                    "Num": [data["drwtNo%d"]%(x) for x in ragne(1, 7)]}
+                    "Num": [data["drwtNo%d"%(x)] for x in range(1, 7)]}
             return info
+
+    def get_all_lotto(self):
+        drwNo = 1
+        while True:
+            info = self.get_lotto(drwNo)
+            if self.log_level == "DEBUG":
+                print("[*] Request %d Lotto Data"%(drwNo))
+            if info:
+                self.lotto_data[drwNo] = info
+            else:
+                break
+            drwNo += 1
 
     def set_log_level(self, log_level):
         if log_level not in self.log_level_list:
             print("level must be ", self.log_level_list)
         self.log_level = log_level
+
+    def save_lotto_data(self, filename = None):
+        self.saver.save_lotto_data(self.lotto_data, filename)
+
+    def load_lotto_data(self, filename = None):
+        self.lotto_data = self.saver.load_lotto_data(filename)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from LottoManager import LottoManager
 
-manager = LottoManager()
+manager = LottoManager("DEBUG")
 print("LottoManager Version = ", manager.__version__)
+manager.get_all_lotto()
+manager.save_lotto_data()
